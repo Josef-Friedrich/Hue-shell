d76adf1828c5f588edaff3754817780bcb2c062c
Josef Friedrich <jf@josef-friedrich.de>
2014-07-04 23:40:22 +0200

    More documentation.

--- sync.sh
+++ sync.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
+# TP link
 rsync -av --delete /home/jf/git-repositories/Hue-shell/ root@192.168.2.4:/root/hue/
+
+# CoovaAP
+scp -r /home/jf/git-repositories/Hue-shell/ root@192.168.2.99:/root/
+
