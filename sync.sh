243e5148246a78ed3eeed07b6f37816916b4530d
Josef Friedrich <jf@josef-friedrich.de>
2014-07-13 13:21:48 +0200

    Change tr to IFS

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # TP link
-rsync -av --delete /home/jf/git-repositories/Hue-shell/ root@192.168.2.4:/root/hue/
+scp -r /home/jf/git-repositories/Hue-shell/ root@192.168.2.4:/root/
 
 # CoovaAP
 scp -r /home/jf/git-repositories/Hue-shell/ root@192.168.2.99:/root/
