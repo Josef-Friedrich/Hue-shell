9740ea76256faef36341bb9c64e50303caa476ac
Josef Friedrich <jf@josef-friedrich.de>
2014-07-16 23:16:02 +0200

    Random.

--- random-generator.sh
+++ random-generator.sh
@@ -1,6 +1,16 @@
 #! /bin/sh
 
+RANDOM="0"
+
+# http://rosettacode.org/wiki/Linear_congruential_generator
+# The Microsoft version
+
+SECONDS=$(date +%s)
+
 while true; do
-  RANDOM=$((214013 * $RANDOM + 2531011 % 2147483648))
+  RANDOM=$((((214013 * $RANDOM) + 2531011 + $SECONDS) % 2147483648))
+  RANDOM=$(($RANDOM / 65536))
   echo $RANDOM
 done
+
+
