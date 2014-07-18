7fdd3d6bae72df069e6c0f359e3ce5574dc45918
Josef Friedrich <jf@josef-friedrich.de>
2014-07-18 12:11:45 +0200

    Implement linear congretual pseudo number generator.

--- random-generator.sh
+++ random-generator.sh
@@ -1,16 +1,27 @@
 #! /bin/sh
 
-RANDOM="0"
+SEED=0
 
 # http://rosettacode.org/wiki/Linear_congruential_generator
 # The Microsoft version
 
-SECONDS=$(date +%s)
+SEED=$(date +%S)
+
+START=1
+END=5
+
+END=$((END + 1))
+
+RANGE=$((END - START))
+
 
 while true; do
-  RANDOM=$((((214013 * $RANDOM) + 2531011 + $SECONDS) % 2147483648))
-  RANDOM=$(($RANDOM / 65536))
-  echo $RANDOM
+  # 2**31 -> 2147483648
+  SEED=$(((214013 * $SEED + 2531011) % 2147483648))
+  # 2**14 -> 16384
+  RANDOM=$(($SEED / 16384))
+  NUMBER_IN_RANGE=$((RANDOM % RANGE))
+  echo $((NUMBER_IN_RANGE + START))
 done
 
 
