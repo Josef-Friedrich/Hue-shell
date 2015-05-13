6459c5cecb0af100ce39c8d7a60f1adf19c7f4cb
Josef Friedrich <jf@josef-friedrich.de>
2015-05-13 07:18:05 +0200

    Pimp up install.sh and uninstall.sh.

--- install.sh
+++ install.sh
@@ -2,16 +2,22 @@
 
 INSTALL_DIR='/usr'
 
+if cp -v README.md /tmp/hue-shell-test-cp > /dev/null 2>&1 ; then
+	CP='cp -v'
+else
+	CP='cp'
+fi
+
 # etc
 mkdir -p /etc/hue-shell
-cp -r config/* /etc/hue-shell
+$CP -r config/* /etc/hue-shell
 
 # lib
 mkdir -p $INSTALL_DIR/lib/hue-shell
-cp base.sh $INSTALL_DIR/lib/hue-shell
+$CP base.sh $INSTALL_DIR/lib/hue-shell
 
 # bin
-cp bin/hue* $INSTALL_DIR/bin
+$CP bin/hue* $INSTALL_DIR/bin
 
 # run
 RUN='/var/run/hue-shell'
@@ -20,12 +26,19 @@ RUN_FILES="$RUN/hue-shell.pids $RUN/hue-shell-random.seed"
 touch $RUN_FILES
 chmod 666 $RUN_FILES
 
+# doc
+if [ -d '/usr/share/doc' ]; then
+	DOC='/usr/share/doc/hue-shell'
+	mkdir -p $DOC
+	$CP doc/* $DOC
+fi
+
 # /etc/init.d
-cp startup/SysVinit /etc/init.d/hue
+$CP startup/SysVinit /etc/init.d/hue
 
 # triggerhappy
 if [ -f /etc/triggerhappy/triggers.d ]; then
-	cp triggerhappy/hue.conf /etc/triggerhappy/triggers.d/
+	$CP triggerhappy/hue.conf /etc/triggerhappy/triggers.d/
 fi
 
 # vim: set ts=8 sw=8 sts=8 et :
--- uninstall.sh
+++ uninstall.sh
@@ -2,12 +2,21 @@
 
 INSTALL_DIR='/usr'
 
-rm -rf /etc/hue-shell
-rm -rf $INSTALL_DIR/lib/hue-shell
-rm -f $INSTALL_DIR/bin/hue*
-rm -f /etc/init.d/hue
-rm -rf /var/run/hue-shell
-rm -f /etc/triggerhappy/triggers.d/hue.conf
+cp README.md /tmp/hue-shell-test-cp > /dev/null 2>&1
+
+if rm -v /tmp/hue-shell-test-cp > /dev/null 2>&1 ; then
+	RM='rm -v'
+else
+	RM='rm'
+fi
+
+$RM -rf /etc/hue-shell
+$RM -rf $INSTALL_DIR/lib/hue-shell
+$RM -f $INSTALL_DIR/bin/hue*
+$RM -f /etc/init.d/hue
+$RM -rf /var/run/hue-shell
+$RM -rf /usr/share/doc/hue-shell
+$RM -f /etc/triggerhappy/triggers.d/hue.conf
 
 # vim: set ts=8 sw=8 sts=8 et :
 # sublime: tab_size 8;
