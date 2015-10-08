adfd5ca8e32fba056ea26f6a39a42cdd73f64967
Josef Friedrich <josef@friedrich.rocks>
2015-10-08 16:14:09 +0200

    Make css work.

--- docker-jeklly.sh
+++ docker-jeklly.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
 docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll \
-	  -it -p 127.0.0.1:4000:4000 jekyll/jekyll:pages jekyll serve
+	  -it -p 127.0.0.1:4000:4000 \
+	  jekyll/jekyll:pages \
+	  jekyll serve --baseurl ''
 
