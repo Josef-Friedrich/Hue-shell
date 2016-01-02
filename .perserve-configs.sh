c4fb2e8f1a11a2b42a318a832d68b2ff4c2f08d8
Josef Friedrich <josef@friedrich.rocks>
2016-01-02 12:42:14 +0100

    Readd .perserve-configs.sh. Add a Makefile to easily update the configuration for development purposes.

---
+++ .perserve-configs.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# For development purposes only!
+
+_replace() {
+	sed -i "s;$1;$2;" /etc/hue-shell/hue-shell.conf
+}
+
+_replace 'IP="192.168.1.2"' 'IP="192.168.2.31"'
+_replace 'USERNAME="yourusername"' 'USERNAME="joseffriedrich"' 
+_replace 'ALL_LIGHTS="1,2,3"' 'ALL_LIGHTS="1,2,3,7,8,9"'
+
---
+++ Makefile
@@ -0,0 +1,3 @@
+install:
+	./install.sh
+	./.perserve-configs.sh
