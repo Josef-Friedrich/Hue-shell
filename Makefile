d6b8afb3d0143a62bb14d39f208f4d1ffc8e26e7
Josef Friedrich <josef@friedrich.rocks>
2016-01-16 11:56:46 +0100

    Create install-dev.sh
    This script install-dev.sh combines the files .perserce-configs.sh and
    Makefile. In addition to that on OpenWRT there is no make installed in
    the default configuration.

--- Makefile
+++
@@ -1,3 +0,0 @@
-install:
-	./install.sh
-	./.perserve-configs.sh
--- install-dev.sh
+++ install-dev.sh
@@ -6,6 +6,8 @@ _replace() {
 	sed -i "s;$1;$2;" /etc/hue-shell/hue-shell.conf
 }
 
+./install.sh
+
 _replace 'IP="192.168.1.2"' 'IP="192.168.2.31"'
 _replace 'USERNAME="yourusername"' 'USERNAME="joseffriedrich"' 
 _replace 'ALL_LIGHTS="1,2,3"' 'ALL_LIGHTS="1,2,3,4,5,6,7,8,9"'
