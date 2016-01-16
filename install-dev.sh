fc503e20805ed8a04e3c03b151052f1e781910d6
Josef Friedrich <josef@friedrich.rocks>
2016-01-16 12:18:31 +0100

    Add download section to install-dev.sh script
    The download section makes it possible to install the latest Hue-shell
    dev version using curl and not git. On OpenWRT it is hard to install
    a git environment. Curl instead is easy to obtain.

--- install-dev.sh
+++ install-dev.sh
@@ -2,13 +2,29 @@
 
 # For development purposes only!
 
+IP="192.168.2.31"
+USERNAME="joseffriedrich"
+ALL_LIGHTS="1,2,3,4,5,6,7,8,9"
+
 _replace() {
 	sed -i "s;$1;$2;" /etc/hue-shell/hue-shell.conf
 }
 
+if [ "$1" = "download" ]; then
+	cd /tmp
+	curl -kL -o Hue-shell.tar.gz https://github.com/Josef-Friedrich/Hue-shell/archive/master.tar.gz
+	tar -xzvf Hue-shell.tar.gz
+	cd Hue-shell-master
+fi
+
 ./install.sh
 
-_replace 'IP="192.168.1.2"' 'IP="192.168.2.31"'
-_replace 'USERNAME="yourusername"' 'USERNAME="joseffriedrich"' 
-_replace 'ALL_LIGHTS="1,2,3"' 'ALL_LIGHTS="1,2,3,4,5,6,7,8,9"'
+if [ "$1" = "download" ]; then
+	rm -rf /tmp/Hue-shell-master
+	rm -f /tmp/Hue-shell.tar.gz
+fi
+
+_replace 'IP="192.168.1.2"' "IP=\"$IP\""
+_replace 'USERNAME="yourusername"' "USERNAME=\"$USERNAME\""
+_replace 'ALL_LIGHTS="1,2,3"' "ALL_LIGHTS=\"$ALL_LIGHTS\""
 
