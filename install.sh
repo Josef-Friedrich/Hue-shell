1c573eef3e7bc8ab92c02ead7e6e411da1be9dc7
Josef Friedrich <josef@friedrich.rocks>
2016-05-01 22:23:47 +0200

    Add _sudo

--- install.sh
+++ install.sh
@@ -1,5 +1,32 @@
 #! /bin/sh
 
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Josef-Friedrich/Hue-shell/master/install.sh) install"
+
+
+if type sudo > /dev/null ; then
+	_sudo() {
+		sudo $@
+	}
+else
+	_sudo() {
+		$@
+	}
+fi
+
+if cp -v /etc/hosts /dev/null > /dev/null 2>&1 ; then
+	_cp() {
+		_sudo cp -v $@
+	}
+else
+	_cp() {
+		_sudo cp $@
+	}
+fi
+
+_mkdir() {
+	_sudo mkdir $@
+}
+
 _usage() {
 	echo "Usage: $(basename $0) (install|upgrade|uninstall)"
 }
@@ -14,40 +41,34 @@ _download() {
 _install_base() {
 	. ./config/hue-shell.conf
 
-	if cp -v README.md /tmp/hue-shell-test-cp > /dev/null 2>&1 ; then
-		CP='cp -v'
-	else
-		CP='cp'
-	fi
-
 	# etc
-	mkdir -p $DIR_CONF
+	_sudo mkdir -p $DIR_CONF
 	if [ -f $DIR_CONF/hue-shell.conf ]; then
-	        $CP -f $DIR_CONF/hue-shell.conf $DIR_CONF/hue-shell.conf.bak
+		_cp -f $DIR_CONF/hue-shell.conf $DIR_CONF/hue-shell.conf.bak
 	fi
-	$CP -rf config/* $DIR_CONF
+	_cp -rf config/* $DIR_CONF
 
 	# lib
-	mkdir -p $DIR_LIB
-	$CP base.sh $DIR_LIB
+	_mkdir -p $DIR_LIB
+	_cp base.sh $DIR_LIB
 
 	# bin
-	$CP bin/hue* $DIR_BIN
-	$CP uninstall.sh $DIR_BIN/hue-uninstall
+	_cp bin/hue* $DIR_BIN
+	_cp uninstall.sh $DIR_BIN/hue-uninstall
 
 	# By Hue-shell generated run files that should "survive" reboot.
-	mkdir -p $DIR_RUN_PERM
-	chmod 777 $DIR_RUN_PERM
-	touch $FILE_RANDOM_SEED
-	chmod 666 $FILE_RANDOM_SEED
+	_mkdir -p $DIR_RUN_PERM
+	_sudo chmod 777 $DIR_RUN_PERM
+	_sudo touch $FILE_RANDOM_SEED
+	_sudo chmod 666 $FILE_RANDOM_SEED
 
 	# doc
-	mkdir -p $DIR_DOC
-	$CP doc/* $DIR_DOC
+	_mkdir -p $DIR_DOC
+	_cp doc/* $DIR_DOC
 
 	# log
-	touch $FILE_LOG
-	chmod 666 $FILE_LOG
+	_sudo touch $FILE_LOG
+	_sudo chmod 666 $FILE_LOG
 }
 
 _install_services() {
@@ -55,7 +76,7 @@ _install_services() {
 	if [ -f /etc/openwrt_version ]; then
 		echo "Installing init.d services ..."
 		_install() {
-			$CP service/openwrt.initd/$1 /etc/init.d/hue-$1
+			_cp service/openwrt.initd/$1 /etc/init.d/hue-$1
 			/etc/init.d/hue-$1 enable
 		}
 		_install load-default
@@ -66,7 +87,7 @@ _install_services() {
 	elif command -v systemctl > /dev/null 2>&1; then
 		echo "Installing systemd services ..."
 		_install() {
-			$CP service/systemd/$1 /lib/systemd/system/hue-$1.service
+			_cp service/systemd/$1 /lib/systemd/system/hue-$1.service
 			systemctl enable /lib/systemd/system/hue-$1.service
 		}
 		_install load-default
@@ -77,17 +98,25 @@ _install_services() {
 	elif [ -d '/etc/init.d' ]; then
 		echo "Installing SysVinit services ..."
 		_install() {
-			$CP service/SysVinit/$1 /etc/init.d/$1
+			_cp service/SysVinit/$1 /etc/init.d/$1
 		}
 		_install load-default
 	fi
 }
 
 _install_triggerhappy() {
-	# triggerhappy
 	if [ -d /etc/triggerhappy/triggers.d ]; then
-		$CP triggerhappy/hue-shell.conf /etc/triggerhappy/triggers.d/
+		_cp triggerhappy/hue-shell.conf /etc/triggerhappy/triggers.d/
 	fi
+}
+
+_install() {
+	if [ ! -f ./bin/hue ]; then
+		_download
+	fi
+	_install_base
+	_install_services
+	_install_triggerhappy
 }
 
 _change_settings() {
@@ -161,8 +190,7 @@ _uninstall() {
 case "$1" in
 
 	install)
-		shift
-		_hue_set $@
+		_install
 		break
 		;;
 	upgrade)
